676133efd0f8b762e385c14933fe55d6787e14a5
Ndumiso Mthembu <Ndumiso.Mthembu@dimensiondata.com>
2019-10-05 11:17:06 +0200

    added dp

--- api/user/login.php
+++ api/user/login.php
@@ -1,6 +1,8 @@
 <?php
 include_once '../../config/Database.php';
 include_once '../../models/Users.php';
+include_once '../../models/Company.php';
+include_once '../../models/Image.php';
 
 $data = json_decode(file_get_contents("php://input"));
 
@@ -12,10 +14,19 @@ $Password = $data->password;
 $database = new Database();
 $db = $database->connect();
 
-//Instantiate user object
+//Instantiate  objects
 $user = new Users($db);
+$company = new Company($db);
+$image = new Image($db);
 
 $result = $user->getUserByEmailandPassword($Email,$Password);
+
+$userCompany = $company->getById($result["CompanyId"] );
+$dp = $image->getById($result["UserId"] );
+
+$result["DP"] = $dp;
+$result["Company"] = $userCompany;
+
 echo json_encode($result);
 
 
