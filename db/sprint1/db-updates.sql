79abffed2d5c2c2799428ceaaac6c918b2118b1f
Ndumiso Mthembu <Ndumiso.Mthembu@dimensiondata.com>
2019-10-13 16:44:35 +0200

    db changes

--- db/sprint1/db-updates.sql
+++ db/sprint1/db-updates.sql
@@ -1,8 +1,3 @@
--- 30/09/2019
-ALTER TABLE `users` ADD `TestCol` INT NULL AFTER `StatusId`;
+ALTER TABLE `users` ADD `RoleId` INT(10) NULL AFTER `CompanyId`;
 
--- 30/09/2019
-ALTER TABLE `users` ADD `TestCol2` VARCHAR(225) NULL AFTER `StatusId`;
-
---Then wegonna run this on production when ever we deploying a new version
 
