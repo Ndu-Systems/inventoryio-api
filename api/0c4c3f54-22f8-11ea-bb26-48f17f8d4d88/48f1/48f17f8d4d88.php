6286cdc91ae5ec073a44cfa278fcd83433a56e06
Ndumiso Mthembu <Ndumiso.Mthembu@dimensiondata.com>
2020-01-05 00:15:44 +0200

    invoice values binding done

--- api/0c4c3f54-22f8-11ea-bb26-48f17f8d4d88/48f1/48f17f8d4d88.php
+++ api/0c4c3f54-22f8-11ea-bb26-48f17f8d4d88/48f1/48f17f8d4d88.php
@@ -46,17 +46,44 @@ $prefix = 'INV';
 $heading = 'Invoice';
 $invoiceNo =  $prefix . $order["OrderId"];
 $companyName = $company["Name"];
-$companyCell = $company["TelephoneNumber"];
-$companyEmail = 'info@dell.co.za';
-$companyAddressL1 = 'The Campus, Wembley Building';
-$companyAddressL2 = '57 Sloane St &, Main Rd, Bryanston, 2021';
 $currency = 'R ';
 
 // customer 
+$billedToLabel = '';
+$clientName = '';
 $customer = $order["Customer"];
-$clientName = $customer['Name'];
+if (isset($customer['Name'])) {
+    $clientName = $customer['Name'];
+    $billedToLabel = 'BILLED TO';
+}
 $dateIssued = date('d M Y', strtotime($order["CreateDate"]));
 
+// company address details
+$addressDetials = $company["Address"];
+if (isset($addressDetials)) {
+    $companyAddressL1 = $addressDetials[0]["Value"];
+    $companyAddressL2 = $addressDetials[1]["Value"];
+    $companyAddressL3 = $addressDetials[2]["Value"];
+    $companyCell = $addressDetials[3]["Value"];
+    $companyEmail = $addressDetials[4]["Value"];
+}
+
+// company banking details
+$bankDetials = $company["Bankings"];
+if (isset($bankDetials)) {
+    $bankName = $bankDetials[0]["Value"];
+    $accountNumber = $bankDetials[1]["Value"];
+    $branchCode = $bankDetials[2]["Value"];
+    $accountHolder = $bankDetials[3]["Value"];
+    $PaymentReference = $clientName . $invoiceNo;
+}
+
+//Logo Url
+$image = $company["Images"];
+$logoUrl = null;
+if (isset($image)) {
+    $logoUrl = $image[0]["Url"];
+}
 
 $dueDate = '';
 $dueDateLabel = '';
@@ -71,11 +98,6 @@ if (isset($order["ExpectedDate"])) {
 
 
 
-$accountNumber = '62737294455';
-$bankName = 'FNB';
-$accountHolder = 'Ndu Systems';
-$PaymentReference = $clientName . $invoiceNo;
-$branchCode = '254005';
 
 $hideBorder = 0;
 $fontSizeSmall = 10;
@@ -89,7 +111,9 @@ $pdf->AddPage();
 // add logo and heading
 // $pdf->Image('img/logo.png', 10, 25, 20);
 //  $pdf->Image('img/ndu systems dp.png', 10, 25, 20);
-$pdf->Image('img/Group 10.png', 10, 25, 25);
+if (isset($logoUrl)) {
+    $pdf->Image($logoUrl, 10, 25, 25);
+}
 $pdf->Ln(20);
 $pdf->SetFont('Arial', 'B', 30);
 $pdf->Cell(60, 10, '', $hideBorder, 0);
@@ -98,7 +122,7 @@ $pdf->Cell(100, 10, $heading, $hideBorder, 1);
 // add client details and company
 $pdf->Ln(15);
 $pdf->SetFont('Arial', '', $fontSizeLarge / 2);
-$pdf->Cell(100, 5, 'BILLED TO', $hideBorder, 0);
+$pdf->Cell(100, 5, $billedToLabel, $hideBorder, 0);
 $pdf->Cell(85, 5, 'BILLED BY', $hideBorder, 1);
 
 $pdf->SetFont('Arial', 'BU', $fontSizeLarge);
@@ -122,6 +146,13 @@ $pdf->SetFont('Arial', '', $fontSizeMed); // value small
 $pdf->Cell($firstColSize,  $rowHeigth, $dateIssued, $hideBorder, 0);
 $pdf->Cell($rowTopSpaceMiddle,  $rowHeigth, '', $hideBorder, 0);
 $pdf->Cell($lastColSize,  $rowHeigth, $companyAddressL2, $hideBorder, 1);
+
+$pdf->SetFont('Arial', 'B', $fontSizeMed); // heading small
+$pdf->Cell($firstColSize,  $rowHeigth, 'Date Issued :', $hideBorder, 0);
+$pdf->SetFont('Arial', '', $fontSizeMed); // value small
+$pdf->Cell($firstColSize,  $rowHeigth, $dateIssued, $hideBorder, 0);
+$pdf->Cell($rowTopSpaceMiddle,  $rowHeigth, '', $hideBorder, 0);
+$pdf->Cell($lastColSize,  $rowHeigth, $companyAddressL3, $hideBorder, 1);
 
 // row  top
 $pdf->SetFont('Arial', 'B', $fontSizeMed); // heading small
